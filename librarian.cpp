1e68fb94a0e7fc02749d4317b020787ce67cf431
RIscRIpt <richard@riscript.com>
2018-05-01 16:44:06 +0300

    simplify librarian message recv

--- librarian.cpp
+++ librarian.cpp
@@ -5,7 +5,7 @@
 
 using namespace rrl;
 
-Librarian::Librarian(rrl::Courier &resolver_courier) 
+Librarian::Librarian(Courier &resolver_courier) 
     : resolver_courier(resolver_courier)
 {}
 
@@ -35,22 +35,19 @@ void Librarian::resolve_external_symbols(Courier &courier, Library &library) {
     library.resolve_external_symbols([this, &courier](std::string_view symbol_name) -> uint64_t {
         auto library_name = get_symbol_library(symbol_name);
         msg::ResolveExternalSymbol msg_resolve_symbol;
-        msg_resolve_symbol.body().library = library_name;
-        msg_resolve_symbol.body().symbol = sanitize_symbol(std::string(symbol_name));
+        msg_resolve_symbol.library = library_name;
+        msg_resolve_symbol.symbol = sanitize_symbol(std::string(symbol_name));
         courier.send(msg_resolve_symbol);
-        auto resolved_symbol = courier.receive().cast<msg::ResolvedSymbol>();
-        return resolved_symbol.body().value;
+        return courier.receive<msg::ResolvedSymbol>().value;
     });
 }
 
 void Librarian::reserve_memory_spaces(Courier &courier, Library &library) {
     library.reserve_memory_spaces([&courier](uint64_t size) -> uint64_t {
         msg::ReserveMemorySpace msg_reserve_memory_space;
-        msg_reserve_memory_space.body().first = 0;
-        msg_reserve_memory_space.body().second = size;
+        msg_reserve_memory_space.value = size;
         courier.send(msg_reserve_memory_space);
-        auto reserved_memory = courier.receive().cast<msg::ReservedMemory>();
-        return reserved_memory.body().value;
+        return courier.receive<msg::ReservedMemory>().value;
     });
 }
 
@@ -58,40 +55,40 @@ void Librarian::perform_relocations(Library &library) {
     library.perform_relocations();
 }
 
-void Librarian::commit_memory_spaces(rrl::Courier &courier, Library &library) {
-    library.commit_memory_spaces([&courier](uint64_t address, DWORD protection, std::vector<std::byte> const data) {
+void Librarian::commit_memory_spaces(Courier &courier, Library &library) {
+    library.commit_memory_spaces([&courier](uint64_t address, DWORD protection, std::vector<std::byte> const &data) {
         msg::CommitMemory msg_commit_memory;
-        msg_commit_memory.body().address = address;
-        msg_commit_memory.body().protection = protection;
-        msg_commit_memory.body().memory = data;
+        msg_commit_memory.address = address;
+        msg_commit_memory.protection = protection;
+        msg_commit_memory.memory = data;
         courier.send(msg_commit_memory);
-        courier.receive().cast<msg::OK>();
+        courier.receive<msg::OK>();
     });
 }
 
-void Librarian::export_symbols(rrl::Courier &courier, Library &library) {
+void Librarian::export_symbols(Courier &courier, Library &library) {
     library.export_symbols([&courier](std::string_view symbol_name, uint64_t address) {
         msg::ExportSymbol msg_export_symbol;
-        msg_export_symbol.body().symbol = sanitize_symbol(std::string(symbol_name));
-        msg_export_symbol.body().address = address;
+        msg_export_symbol.symbol = sanitize_symbol(std::string(symbol_name));
+        msg_export_symbol.address = address;
         courier.send(msg_export_symbol);
     });
 }
 
-void Librarian::execute_entry_point(rrl::Courier &courier, Library &library) {
+void Librarian::execute_entry_point(Courier &courier, Library &library) {
     auto entry_point = library.get_entry_point();
     if (entry_point) {
         msg::Execute msg_execute;
-        msg_execute.body().value = entry_point;
+        msg_execute.value = entry_point;
         courier.send(msg_execute);
+        courier.receive<msg::OK>();
     }
 }
 
 std::string Librarian::get_symbol_library(std::string_view symbol_name) {
-    msg::GetSymbolLibrary msg_get_symbol_library;
-    msg_get_symbol_library.body() = symbol_name;
+    msg::GetSymbolLibrary msg_get_symbol_library{std::string(symbol_name)};
     resolver_courier.send(msg_get_symbol_library);
-    return resolver_courier.receive().cast<msg::ResolvedSymbolLibrary>().body() + ".dll";
+    return resolver_courier.receive<msg::ResolvedSymbolLibrary>();
 }
 
 std::string Librarian::sanitize_symbol(std::string const &symbol_name) {
@@ -101,4 +98,3 @@ std::string Librarian::sanitize_symbol(std::string const &symbol_name) {
         return match[2];
     return symbol_name;
 }
-
