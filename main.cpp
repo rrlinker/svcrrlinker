e9d9bfb1339a52baf1951538bcd3fbbf91c0312e
RIscRIpt <richard@riscript.com>
2019-06-26 13:25:45 +0100

    allow using crypto courier by passing 5th argument with key

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include <string>
 #include <algorithm>
 #include <filesystem>
+#include <memory>
 
 #include <unistd.h>
 #include <fcntl.h>
 
 #include <librlcom/raw_courier.hpp>
+#include <librlcrypto/crypto_courier.hpp>
 #include "unix_connection.hpp"
 #include "posix_connection.hpp"
 #include "librarian.hpp"
@@ -16,11 +18,12 @@
 #include "coff.hpp"
 
 using namespace rrl;
+using namespace rrl::rlc;
 
 int main(int argc, const char *argv[]) {
-    if (argc != 4) {
+    if (argc < 4 || argc > 5) {
         const char *program = argc > 0 ? argv[0] : "svclinker";
-        std::cerr << "usage: " << program << " <file descriptor of socket> <symbol resolver unix server> <library path>\n"
+        std::cerr << "usage: " << program << " <file descriptor of socket> <symbol resolver unix server> <library path> [key for crypto courier]\n"
             << "\tLinks (sends) specified library to the socket endpoint." << std::endl;
         return 1;
     }
@@ -29,6 +32,10 @@ int main(int argc, const char *argv[]) {
     int fd = atoi(argv[1]);
     auto resolver_path = std::filesystem::path(argv[2]);
     auto library_path = std::filesystem::path(argv[3]);
+    std::string key;
+    if (argc >= 5) {
+        key = argv[4];
+    }
 
     if (!fd) {
         std::cerr << '`' << argv[1] << '`' << " is not a valid number of a file descriptor.\n";
@@ -60,17 +67,24 @@ int main(int argc, const char *argv[]) {
         UnixConnection resolver_conn;
         resolver_conn.connect(Address{resolver_path});
 
-        RawCourier courier(conn);
+        std::unique_ptr<Courier> courier;
+        if (key.empty()) {
+            courier = std::make_unique<RawCourier>(conn);
+        } else {
+            auto crypto_courier = std::make_unique<CryptoCourier>(conn);
+            crypto_courier->init_as_server(bytes_from_hex_string(key));
+            courier = std::move(crypto_courier);
+        }
         RawCourier resolver_courier(resolver_conn);
 
         msg::OK msg_ok;
-        courier.send(msg_ok);
+        courier->send(msg_ok);
 
         Library library(library_path);
 
         Librarian librarian(resolver_courier);
 
-        librarian.link(courier, library);
+        librarian.link(*courier, library);
     } catch (std::exception const &e) {
         std::cerr << e.what() << '\n';
         return -1;
